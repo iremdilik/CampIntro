debb099b2ec6b207d1bd605a192a033d64b536d6
irem dilik <iremdilik@gmail.com>
2021-03-17 19:30:30 +0300

    Add project files.

---
+++ CampIntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - kendini tekrar etme
+            //değer tutucu, allias
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayısı = 32000;
+            double faizOranı = 1.45;
+            bool sistemeGirişYapmişMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirişYapmişMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu"); ;
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
